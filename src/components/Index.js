3fa9abdc8c876d7414bf79e4d2fe2966d82db558
Fenil Jikadara <146850675+imfeniljikadara@users.noreply.github.com>
2024-07-11 12:12:35 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -34,10 +34,10 @@ const Index = () => {
 
         <main>
           {<div className="py-4">
-              <img src="globe-img.png" className="index-image" alt="Office workspace 1" />
-              <img src="/srs/logo_bg.png" className="index-image1" alt="Office workspace 2" />
-              <img src="/srs/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png" className="index-image2" alt="Office workspace 3" />
-              <img src="/srs/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png" className="index-image3" alt="Office workspace 4" />
+              // <img src="globe-img.png" className="index-image" alt="Office workspace 1" />
+              // <img src="/srs/logo_bg.png" className="index-image1" alt="Office workspace 2" />
+              // <img src="/srs/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png" className="index-image2" alt="Office workspace 3" />
+              // <img src="/srs/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png" className="index-image3" alt="Office workspace 4" />
             {/* // <img src={image1} alt="Office workspace 1" className="index-image" />
             // <img src={image2} alt="Office workspace 2" className="index-image1" />
             // <img src={image3} alt="Office workspace 3" className="index-image2" />
