7880b60dce9e581721b761439945961e24f28a88
Fenil Jikadara <146850675+imfeniljikadara@users.noreply.github.com>
2024-07-11 12:15:50 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -38,10 +38,10 @@ const Index = () => {
               // <img src="/srs/logo_bg.png" className="index-image1" alt="Office workspace 2" />
               // <img src="/srs/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png" className="index-image2" alt="Office workspace 3" />
               // <img src="/srs/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png" className="index-image3" alt="Office workspace 4" />
-            {/* // <img src={image1} alt="Office workspace 1" className="index-image" />
-            // <img src={image2} alt="Office workspace 2" className="index-image1" />
-            // <img src={image3} alt="Office workspace 3" className="index-image2" />
-            // <img src={image4} alt="Office workspace 4" className="index-image3" /> */}
+            <img src={/srs/globe-img.png} alt="Office workspace 1" className="index-image" />
+            <img src={/srs/logo_bg.png} alt="Office workspace 2" className="index-image1" />
+            <img src=/srs/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png} alt="Office workspace 3" className="index-image2" />
+            <img src={srs/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png} alt="Office workspace 4" className="index-image3" />
           </div>}
         </main>
 
