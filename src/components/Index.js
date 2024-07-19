fa7c8256517567952e7d6064c40f26148f5f6736
Fenil Jikadara <146850675+imfeniljikadara@users.noreply.github.com>
2024-07-19 12:11:41 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import { Container, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 // import SearchComponent from './SearchComponent';
 import './Index.css'; // Custom CSS for Index page
-import image1 from '/src/globe-img.png';
-import image2 from '/src/logo_bg.png';
-import image3 from '/src/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png';
-import image4 from '/src/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png';
+// import image1 from '/src/globe-img.png';
+// import image2 from '/src/logo_bg.png';
+// import image3 from '/src/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png';
+// import image4 from '/src/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png';
 const Index = () => {
   return (
     <div>
@@ -34,14 +34,14 @@ const Index = () => {
 
         <main>
           <div className="py-4">
-              {/* <img src="/src/globe-img.png" className="index-image" alt="Office workspace 1" />
+              <img src="/src/globe-img.png" className="index-image" alt="Office workspace 1" />
               <img src="/srs/logo_bg.png" className="index-image1" alt="Office workspace 2" />
               <img src="/srs/Screenshot_2024-07-10_at_4.06.00_PM-removebg-preview.png" className="index-image2" alt="Office workspace 3" />
               <img src="/srs/Screenshot_2024-07-10_at_4.06.09_PM-removebg-preview.png" className="index-image3" alt="Office workspace 4" /> */}
-            <img src={image1} alt="Office workspace 1" className="index-image" />
-            <img src={image2} alt="Office workspace 2" className="index-image1" />
-            <img src={image3} alt="Office workspace 3" className="index-image2" />
-            <img src={image4} alt="Office workspace 4" className="index-image3" />
+            // <img src={image1} alt="Office workspace 1" className="index-image" />
+            // <img src={image2} alt="Office workspace 2" className="index-image1" />
+            // <img src={image3} alt="Office workspace 3" className="index-image2" />
+            // <img src={image4} alt="Office workspace 4" className="index-image3" />
           </div>
         </main>
 
